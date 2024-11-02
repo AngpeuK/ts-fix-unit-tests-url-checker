cf978de5fc352d389c685e8ee85880ed7c30f77b
andrei <angpeu@gmail.com>
2024-11-02 18:11:55 +0200

    yml test fixed

--- tests/url-checker.test.ts
+++ tests/url-checker.test.ts
@@ -12,24 +12,25 @@ describe('LinkChecker', () => {
         expect(linkChecker.isValidUrl('https://www.example.com')).toBe(true);
     })
 
-    // FIXME
+    // FIXED
     test('should return true for valid URL with http', () => {
-        expect(linkChecker.isValidUrl('http://www.example.com'))
+        expect(linkChecker.isValidUrl('http://www.example.com')).toBeTruthy()
+        //console.log(linkChecker.isValidUrl)
     })
 
-    // FIXME
+    // FIXED
     test('should return false for invalid URL with no protocol', () => {
         expect(linkChecker.isValidUrl('www.example.com')).toBeFalsy()
     })
 
-    // FIXME:
+    // FIXED
 
-    // test('should return true for valid URL with port', () => {
-    //     linkChecker.isValidUrl('http://www.example.com:8080')).toBetrue;
-    // })
-    //
-    // test('should return true for valid URL with path', () => {
-    //     expect(linkCheckker.isValidUrl('http://www.example.com/path/to/page')).toBeFalsy()
-    // })
+    test('should return true for valid URL with port', () => {
+        expect(linkChecker.isValidUrl('http://www.example.com:8080')).toBeTruthy();
+    })
+
+    test('should return true for valid URL with path', () => {
+        expect(linkChecker.isValidUrl('http://www.example.com/path/to/page')).toBeTruthy()
+    })
 
 })
